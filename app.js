cbfff10693ff5f60ae36487aee80f1c4e96b5e0d
bill-i-am-code <69911640+bill-i-am-code@users.noreply.github.com>
2020-11-24 15:58:28 +0000

    End of project

---
+++ app.js
@@ -0,0 +1,11 @@
+const heading = 'Please Meet Out Team'
+let i = 0;
+console.log(heading.length);
+const typing = () =>{
+    if(i<heading.length){
+        document.querySelector('.heading').innerHTML += heading.charAt(i);
+        i++;
+        setTimeout(typing, 150);
+    }
+}
+typing();
